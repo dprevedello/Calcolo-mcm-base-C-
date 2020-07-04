5c1d34a4976770da3562af388f2b5a1322a303d6
Daniele Prevedello <prevedello@altervista.org>
2020-07-04 09:06:12 +0000

    mcm nelle printf

--- main.c
+++ main.c
@@ -6,17 +6,17 @@ int main(void) {
     int numero1, numero2;
 
     do {
-        printf("Inserire il primo numero con il qualse si vuole calcolare l'MCM: ");
+        printf("Inserire il primo numero con il qualse si vuole calcolare l'mcm: ");
         scanf("%d", &numero1);
         if(numero1 <= 0)
-            printf("ERRORE: per il calcolo dell'MCM il numero deve essere strettamente positivo.\n\n");
+            printf("ERRORE: per il calcolo dell'mcm il numero deve essere strettamente positivo.\n\n");
     } while(numero1 <= 0);
 
     do {
-        printf("Inserire il secondo numero con il qualse si vuole calcolare l'MCM: ");
+        printf("Inserire il secondo numero con il qualse si vuole calcolare l'mcm: ");
         scanf("%d", &numero2);
         if(numero2 <= 0)
-            printf("ERRORE: per il calcolo dell'MCM il numero deve essere strettamente positivo.\n\n");
+            printf("ERRORE: per il calcolo dell'mcm il numero deve essere strettamente positivo.\n\n");
     } while(numero2 <= 0);
 
     printf("\nIl minimo comune multiplo fra %d e %d è %d\n", numero1, numero2, mcm(numero1, numero2));
